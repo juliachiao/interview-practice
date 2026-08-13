94648aea8996eb176dc44ebfca5ce821fad3acaf
20250318 <julia.jih.0507@gmail.com>
2026-08-13 16:12:08 +0800

    Update feedback.js

--- api/feedback.js
+++ api/feedback.js
@@ -19,7 +19,7 @@ module.exports = async function handler(req, res) {
 
   try {
     const geminiResponse = await fetch(
-      'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent',
+      'https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash-lite:generateContent',
       {
         method: 'POST',
         headers: {
